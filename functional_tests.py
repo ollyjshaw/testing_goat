d524505bf5ae8ec0596c883fc98574851cace339
Olly Shaw <ollyjshaw@gmail.com>
2018-06-16 17:03:10 +0100

    end chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,7 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -11,21 +12,26 @@ class NewVisitorTest(unittest.TestCase):
         options.set_headless(headless=True)
         self.browser = webdriver.Chrome(options=options)
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -33,24 +39,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')  
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)  
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        self.fail('Finish the test!') 
+        # There is still a text box inviting her to add another item. She
+        #    enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-if __name__ == '__main__':  
-    unittest.main(warnings='ignore')  
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- notes.md
+++ notes.md
@@ -2,5 +2,11 @@ I decided to use ChromeDriver instead
 
 brew cask install chromedriver
 
-good explaination of list generators
+good explanation of list generators
 https://medium.freecodecamp.org/python-list-comprehensions-vs-generator-expressions-cef70ccb49db
+
+
+you can put in pytest to run the unit tests
+then you can continually watch with ptw
+
+https://github.com/joeyespo/pytest-watch
