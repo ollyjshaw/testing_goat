6536db371778872621b8e4fb00163095df289134
Olly Shaw <ollyjshaw@gmail.com>
2018-06-16 11:09:25 +0100

    some linting

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,11 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page  
-from django.http import HttpRequest
+
 
 class SmokeTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')  
-        html = response.content.decode('utf8')  
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
