eeda322c5babe01eb77023ca02b36b2c203abda6
Olly Shaw <ollyjshaw@gmail.com>
2018-06-17 20:21:02 +0100

    midway working incrementally

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+    return render(request, 'home.html')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
--- notes.md
+++ notes.md
@@ -10,3 +10,9 @@ you can put in pytest to run the unit tests
 then you can continually watch with ptw
 
 https://github.com/joeyespo/pytest-watch
+
+
+here's how the max concurrency is set
+https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/AuroraPostgreSQL.Managing.html
+
+Looking at gunicorn threads and processes. Should be able to support 100 connections for a small db.
